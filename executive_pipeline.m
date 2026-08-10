8cf6fc628d4d2684202c148581056f0b552801be
Goldstein Léo <leo.goldstein-abecassis@inserm.fr>
2026-08-11 00:20:01 +0200

    compléxification du recalage

--- executive_pipeline.m
+++ executive_pipeline.m
@@ -1,8 +1,8 @@
 %% Choix du type
-% clearvars -except choices group_order selected_groups
-% clc
-clear 
+clearvars -except choices group_order selected_groups
 clc
+%clear 
+%clc
 
 setup_python_env()
 
@@ -15,7 +15,7 @@ setup_python_env()
 if ~exist('selected_groups','var')
     selected_groups = [];
 end
-
+%%
 [selected_groups, animal_date_list] = folder_selection(choices, group_order, dataFolders_by_group, selected_groups);
 
 output_folders = build_output_folders(selected_groups, root_folders, automatic_selection, include_electroporated);
@@ -25,14 +25,14 @@ selected_groups = create_data(selected_groups);
 [selected_groups, metadata_table] = create_metadata(selected_groups);
 
 %recap_all = create_summary_sheets(selected_gr0oups);
-
+%%
 % Data processing
 selected_groups = process_selected_groups(selected_groups, include_electroporated);
 %%
 [selected_groups, output_folders] = DF_peak_detection(selected_groups, include_electroporated, automatic_selection, output_folders);
 
 %selected_groups = data_checking(selected_groups, include_electroporated);
-%%
+
 [selected_groups, results_table] = compute_DF(selected_groups, include_electroporated);
 
 [ ...
